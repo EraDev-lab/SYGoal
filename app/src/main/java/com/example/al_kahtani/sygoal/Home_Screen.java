7adc0b30a61a3150ea4c5af00ed28bdab5046275
Your Name <you@example.com>
2019-05-02 20:21:38 +0200

    doaa

--- app/src/main/java/com/example/al_kahtani/sygoal/Home_Screen.java
+++ app/src/main/java/com/example/al_kahtani/sygoal/Home_Screen.java
@@ -2,6 +2,7 @@ package com.example.al_kahtani.sygoal;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -14,60 +15,29 @@ import com.example.al_kahtani.sygoal.classes.SharedPref;
 import java.util.Locale;
 
 public class Home_Screen extends AppCompatActivity {
-    SharedPref sharedpref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        sharedpref = new SharedPref(this);//load night mode setting
-        if(sharedpref.loadNightModeState()==true) {
-            setTheme(R.style.darktheme);
-        }else{  setTheme(R.style.AppTheme);}
-        loadLocale();//load languge setting
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        Thread thread = new Thread(){
+
+            @Override
+
+
+            public  void  run() {
+
+                try {
+                     sleep(3000);
+
+                    Intent intent = new Intent(getApplicationContext(),BottomNavigationViewActivity.class);
+                    startActivity(intent);
+
+                }
+                catch (InterruptedException e){
+                 e.printStackTrace();
+                }
+            }
+         };
     }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home__screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar achieve_and_missed_item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    // languge setting
-    public void setLocale(String lang) {
-        Locale locale = new Locale(lang);
-        Locale.setDefault(locale);
-        Configuration configuration = new Configuration();
-        configuration.locale = locale;
-        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
-        SharedPreferences.Editor editor = getSharedPreferences("SettingActivity", Context.MODE_PRIVATE).edit();
-        editor.putString("My_Lang", lang);
-        editor.apply();
-
-    }
-
-    public void loadLocale() {
-        SharedPreferences pref = getSharedPreferences("SettingActivity", Activity.MODE_PRIVATE);
-        String language = pref.getString("My_Lang", "");
-        setLocale(language);
-    }
-
-
 }
