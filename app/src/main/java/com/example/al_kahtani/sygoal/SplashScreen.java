a80d07daf5bbc729e6ee41c3bda337bab7547f15
hamzama06 <hamzamani0606@gmail.com>
2019-05-08 13:50:21 +0100

    add rotate animation

--- app/src/main/java/com/example/al_kahtani/sygoal/SplashScreen.java
+++ app/src/main/java/com/example/al_kahtani/sygoal/SplashScreen.java
@@ -1,8 +1,11 @@
 package com.example.al_kahtani.sygoal;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,11 +13,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView splashLogo = findViewById(R.id.splash_logo);
+
+        // add rotate animation
+        Animation rotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_animation);
+        splashLogo.startAnimation(rotateAnimation);
+
         Thread timer =new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
